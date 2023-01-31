65148d5fd0dbc110a375727f5fe572be17e06de5
Anika Mahajan <anikamahajan.usa@gmail.com>
2023-01-30 17:27:29 -0800

    slow down moon

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -92,7 +92,7 @@ function nightSkySketch(p) {
 
         // controls how much of moon gets filled
         if (i % 100 == 0) {
-            cScale += 0.05;
+            cScale += 0.02;
         }
 
         angle += 0.0005;
