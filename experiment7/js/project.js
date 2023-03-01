4e779f7010be0bf26bc3f2c35963247f29274b0f
Anika Mahajan <anikamahajan.usa@gmail.com>
2023-02-28 23:45:23 -0800

    comments

--- experiment7/js/project.js
+++ experiment7/js/project.js
@@ -1,6 +1,12 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Anika Mahajan
+// Date: 03/02/2023
+// Purpose: Experiment 7 - Data Visualization
+// for CMPM 169 taught by Wes Modes at UCSC Winter 2023
+
+// Code Attributions
+// basis for word cloud: https://d3-graph-gallery.com/graph/wordcloud_basic.html
+// basis for filtering words: https://observablehq.com/@d3/word-cloud
+// basis for filtering array and counting appearances of each item: https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
 
 // importing JSON
 import messages from './../json/cardifferent.js';
@@ -8,8 +14,11 @@ import messages from './../json/cardifferent.js';
 let messageString = "";
 let stopwords = new Set("i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall,it'll".split(","))
 
+// regex's
 let numRegex = /\d+/g;
 let letterRegex = /^[a-zA-Z]$/g;
+
+// loops through messages and filters and adds to one giant string
 messages.messages.forEach(function (m) {
   if (m.content) {
     if (!m.content.includes(".com")) {
@@ -29,6 +38,7 @@ messages.messages.forEach(function (m) {
 
 });
 
+// giant string is split by space and then filtered again
 let messageWords = messageString.split(/[\s.]+/g)
   .map(w => w.replace(/^[“‘"\-—()\[\]{}*#|]+/g, ""))
   .map(w => w.replace(/[;:.!?()\[\]{},"'’”\-—]+$/g, ""))
@@ -42,6 +52,7 @@ let messageWords = messageString.split(/[\s.]+/g)
   .filter(w => !w.includes("^"))
   .filter(w => !letterRegex.test(w));
 
+// turn array into an object where keys are word and values are how many times that word appears
 const counts = {};
 messageWords.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
 
@@ -52,7 +63,6 @@ let margin = { top: 10, right: 10, bottom: 10, left: 10 },
 
 // append the svg object to the body of the page
 let svg;
-
 let layout;
 
 // This function takes the output of 'layout' above and draw the words
@@ -74,16 +84,14 @@ function draw(words) {
 
 
 function main() {
-  // console.log(counts);
-  console.log(Object.keys(counts).length);
+  // filters words that appear less than 7 times and are only 1 letter
   for (const key in counts) {
     if (counts[key] < 7 || key.length < 2) {
       delete counts[key];
     }
   }
-  console.log(Object.keys(counts).length);
-  console.log(counts);
 
+  // creates space for word cloud on page
   svg = d3.select("#wordCloud").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -93,6 +101,7 @@ function main() {
     "translate(" + margin.left + "," + margin.top + ")");
 
   // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+  // font size is based on how many counts there are of the word
   layout = d3.layout.cloud()
     .size([width, height])
     .words(Object.keys(counts).map(function (d) { return { text: d, size: counts[d] }; }))
@@ -103,10 +112,12 @@ function main() {
   layout.start();
 }
 
+// run code on load
 window.onload = function () {
   main();
 };
 
+// run code when button is clicked
 document.getElementById("redraw").onclick = function() {
   d3.select("svg").remove();
 	main();
--- experiment7/node_app/app.js
+++ experiment7/node_app/app.js
@@ -1,3 +1,11 @@
+// Author: Anika Mahajan
+// Date: 03/02/2023
+// Purpose: Experiment 7 - Data Visualization
+// for CMPM 169 taught by Wes Modes at UCSC Winter 2023
+
+// Code Attributions
+// set up for node app: https://www.geeksforgeeks.org/how-to-create-and-run-node-js-project-in-vs-code-editor/
+
 // Requiring module
 const express = require('express');
  
@@ -21,23 +29,29 @@ app.listen(PORT,console.log(
   fs = require('fs');
 let name = '../json/cardifferent.json';
 let m = JSON.parse(fs.readFileSync(name).toString());
+
+// edits the json file permanently
 m.messages.forEach(function(p){
+    // takes out all names
     p.sender_name = "REDACTED";
     if(p.users) {
         p.users.forEach(function(q){
             q.name = "REDACTED";
         });
     }
+    if(p.reactions){
+        p.reactions.forEach(function(q){
+            q.actor = "REDACTED";
+        });
+    }
+
+    // removes any @ signs
     if(p.content) {
         if(p.content.includes("@")) {
             console.log("reach");
             p.content = p.content.replace("@", "");
         }
     }
-    if(p.reactions){
-        p.reactions.forEach(function(q){
-            q.actor = "REDACTED";
-        });
-    }
+    
 });
 fs.writeFileSync(name, JSON.stringify(m));
